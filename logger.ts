ee134840b1c6f1f08a43a03238c023279c55d27a
Miroslav Popov <info@forexsb.com>
2024-10-17 00:40:39 +0300

    first commit

---
+++ logger.ts
@@ -0,0 +1,208 @@
+import { appendText } from "@popov/file-writer";
+
+type Message = Error | object | string | number;
+
+const EOL = Deno.build.os === "windows" ? "\r\n" : "\n";
+
+export interface LoggerOptions {
+  tee?: boolean;
+  suppress?: string[];
+}
+
+const loggerOptions: LoggerOptions = {
+  tee: false,
+  suppress: [],
+};
+
+let logPath = "";
+let isInit = false;
+let lastError: string | null = null;
+
+const tagMap: Record<string, string> = {
+  debug: "[DEBUG]",
+  error: "[ERROR]",
+  warning: "[WARNING]",
+  info: "[INFO]",
+  success: "[SUCCESS]",
+  text: "",
+};
+
+const colors: Record<string, string> = {
+  reset: "\x1b[0m", // reset
+  debug: "\x1b[33m", // yellow
+  warning: "\x1b[33m", // yellow
+  error: "\x1b[31m", // red
+  info: "",
+  success: "\x1b[32m", // green
+  text: "",
+};
+
+/**
+ * Initializes the logger with the path to the log file.
+ * 
+ * Options: { tee: boolean, suppress: string[] }
+ * 
+ * If `tee` is true, the logger will write to the console and the log file.
+ * 
+ * If `suppress` is an array of strings, the logger will not log the messages with these tags.
+ */
+export function initLogger(
+  logFilePath?: string,
+  options?: LoggerOptions,
+): void {
+  if (logFilePath) {
+    logPath = logFilePath;
+  }
+
+  if (options?.tee) {
+    loggerOptions.tee = options.tee;
+  }
+
+  if (Array.isArray(options?.suppress)) {
+    loggerOptions.suppress = options.suppress;
+  }
+
+  isInit = true;
+
+  if (logPath === "") {
+    return;
+  }
+
+  try {
+    const lstat: Deno.FileInfo = Deno.lstatSync(logPath);
+    if (!lstat.isFile) {
+      // noinspection ExceptionCaughtLocallyJS
+      throw new Error("Log path is not a file.");
+    }
+  } catch (err) {
+    if (err instanceof Deno.errors.NotFound) {
+      Deno.createSync(logPath);
+    } else {
+      throw err;
+    }
+  }
+}
+
+/**
+ * Logs an error message with tag [ERROR] in Red.
+ * 
+ * If `tee` is true, the logger will write to the `stderr`.
+ */
+export function logError(message: Message, sender?: string): void {
+  lastError = stringifyMessage(message);
+  logMessage("error", message, sender);
+}
+
+/**
+ * Logs a debug message with tag [DEBUG] in Yellow.
+ */
+export function logDebug(message: Message, sender?: string): void {
+  logMessage("debug", message, sender);
+}
+
+/**
+ * Logs a warning message with tag [WARNING] in Yellow.
+ */
+export function logWarning(message: Message, sender?: string): void {
+  logMessage("warning", message, sender);
+}
+
+/**
+ * Logs an info message with tag [INFO].
+ */
+export function logInfo(message: Message, sender?: string): void {
+  logMessage("info", message, sender);
+}
+
+/**
+ * Logs a success message with tag [SUCCESS] in Green.
+ */
+export function logSuccess(message: Message, sender?: string): void {
+  logMessage("success", message, sender);
+}
+
+/**
+ * Logs a text message.
+ */
+export function logText(message: Message): void {
+  logMessage("text", message, "");
+}
+
+/**
+ * Returns the last logged error message.
+ */
+export function getLastError(): Error | string | null {
+  return lastError;
+}
+
+/**
+ * Resets the last logged error message.
+ */
+export function resetLastError(value?: null): void {
+  lastError = value || null;
+}
+
+function logMessage(tag: string, message: Message, sender?: string): void {
+  if (loggerOptions.suppress && loggerOptions.suppress.includes(tag)) {
+    return;
+  }
+
+  const text: string =
+    ["info", "error", "debug", "warning", "success"].includes(tag)
+      ? composeMessage(tag, message, sender)
+      : String(message);
+
+  if (isInit && logPath !== "") {
+    try {
+      appendText(logPath, text + EOL);
+    } catch (err) {
+      console.error((err as Error).message);
+    }
+  }
+
+  if (!isInit || loggerOptions.tee || logPath === "") {
+    const msg = colors[tag] + text + colors.reset;
+    if (tag === "error") {
+      console.error(msg);
+    } else {
+      console.log(msg);
+    }
+  }
+}
+
+function composeMessage(
+  tag: string,
+  message: Message,
+  sender?: string,
+): string {
+  const timeText = getLocalTimeText();
+  const senderText = sender ? `[${sender}] ` : "";
+  const messageText = stringifyMessage(message);
+
+  return `${timeText} ${tagMap[tag]} ${senderText}${messageText}`;
+}
+
+function stringifyMessage(message: Message): string {
+  if (typeof message === "object") {
+    if (message instanceof Error) {
+      return message.message;
+    }
+
+    return JSON.stringify(message, null, 2);
+  }
+
+  return String(message);
+}
+
+function getLocalTimeText(): string {
+  const now = new Date();
+
+  const year = now.getFullYear().toString();
+  const month = ("0" + (now.getMonth() + 1)).slice(-2);
+  const day = ("0" + now.getDate()).slice(-2);
+  const hours = ("0" + now.getHours()).slice(-2);
+  const minutes = ("0" + now.getMinutes()).slice(-2);
+  const seconds = ("0" + now.getSeconds()).slice(-2);
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
---
+++ logger_test.ts
@@ -0,0 +1,69 @@
+import { ok, strictEqual } from "node:assert";
+import { stderr, stdout } from "node:process";
+
+import {
+  getLastError,
+  initLogger,
+  logDebug,
+  logError,
+  logInfo,
+  logSuccess,
+  logText,
+  logWarning,
+} from "./mod.ts";
+
+
+initLogger("", { suppress: [], tee: true });
+
+let message = "";
+
+console.log = (msg: string) => {
+  message = msg;
+  stdout.write(msg + "\n");
+};
+
+console.error = (msg: string) => {
+  message = msg;
+  stderr.write(msg + "\n");
+};
+
+Deno.test("logError", () => {
+  logError("error message", "logger_test.ts");
+  ok(message.includes("[ERROR]"));
+  ok(message.includes("error message"));
+  ok(message.includes("logger_test.ts"));
+  strictEqual(getLastError(), "error message");
+});
+
+Deno.test("logDebug", () => {
+  logDebug("debug message", "logger_test.ts");
+  ok(message.includes("[DEBUG]"));
+  ok(message.includes("debug message"));
+  ok(message.includes("logger_test.ts"));
+});
+
+Deno.test("logWarning", () => {
+  logWarning("warning message", "logger_test.ts");
+  ok(message.includes("[WARNING]"));
+  ok(message.includes("warning message"));
+  ok(message.includes("logger_test.ts"));
+});
+
+Deno.test("logInfo", () => {
+  logInfo("info message", "logger_test.ts");
+  ok(message.includes("[INFO]"));
+  ok(message.includes("info message"));
+  ok(message.includes("logger_test.ts"));
+});
+
+Deno.test("logSuccess", () => {
+  logSuccess("success message", "logger_test.ts");
+  ok(message.includes("[SUCCESS]"));
+  ok(message.includes("success message"));
+  ok(message.includes("logger_test.ts"));
+});
+
+Deno.test("logText", () => {
+  logText("text message");
+  ok(message.includes("text message"));
+});
